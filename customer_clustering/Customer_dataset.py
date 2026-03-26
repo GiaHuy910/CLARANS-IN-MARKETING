cc8055f2d353c42ac0e6866c2752d9fdef77a40c
GiaHuy910 <nguyenbui.09101999@gmail.com>
2026-03-26 10:33:44 +0700

    half done

---
+++ customer_clustering/Customer_dataset.py
@@ -0,0 +1,7 @@
+import pandas as pd
+path=r'../customer_clustering/datasets/customer_segmentation_data.csv'
+data=pd.read_csv(path)
+
+data=data.drop(columns=['Customer_ID','Segmentation_Group','Purchase_History','Marital_Status','Education_Level','Geographic_Information','Occupation','Behavioral_Data','Interactions_with_Customer_Service','Insurance_Products_Owned','Coverage_Amount','Premium_Amount','Policy_Type','Customer_Preferences','Preferred_Communication_Channel','Preferred_Contact_Time','Preferred_Language'])
+data = data.dropna()
+data=pd.get_dummies(data,dtype=int,drop_first=True)
--- dataset_downloader.py
+++
@@ -1,6 +0,0 @@
-import kagglehub
-
-# Chạy file này nếu cần tải dataset
-path = kagglehub.dataset_download("ravalsmit/customer-segmentation-data")
-
-print("Path to dataset files:", path)
